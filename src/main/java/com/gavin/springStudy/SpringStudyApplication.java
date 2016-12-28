5403c2c7372c6fa8725ae32a16f20250006e48d4
Li Gaozhao <ligaozhao@icloud.com>
2016-12-28 15:27:00 +0800

    Spring example about init and destroy.

--- src/main/java/com/gavin/springStudy/SpringStudyApplication.java
+++ src/main/java/com/gavin/springStudy/SpringStudyApplication.java
@@ -2,11 +2,20 @@ package com.gavin.springStudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.gavin.springStudy.bean.Message;
 
 @SpringBootApplication
 public class SpringStudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringStudyApplication.class, args);
+//		SpringApplication.run(SpringStudyApplication.class, args);
+		
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		Message msg = (Message) context.getBean("message");
+		msg.getMessage();
+		context.registerShutdownHook();
 	}
 }
---
+++ src/main/java/com/gavin/springStudy/beanPostProcessor/initMessage.java
@@ -0,0 +1,23 @@
+package com.gavin.springStudy.beanPostProcessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class initMessage implements BeanPostProcessor{
+	
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("BeforeInitialization : " + beanName);
+		return bean; // you can return any other object as well
+	}
+	
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("AfterInitialization : " + beanName);
+		return bean;
+	}
+
+	
+	
+
+}
